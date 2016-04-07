34d9caae58151b1892f50bcecb4719cd7a75bf53
mdimarco <mhd894@gmail.com>
2016-04-07 19:09:15 -0400

    Place bet includes full size image

--- client/views/feed/Feed.jsx
+++ client/views/feed/Feed.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 class Image extends React.Component {
 	render() {
 		return (
-			<div className="uk-width-medium-1-3"> 
+			<div className="uk-width-small-1-3 uk-width-1-2"> 
 				<div className="uk-thumbnail">
 					<figure className="uk-overlay">
-                        <img width="600" height="400" src="http://memesvault.com/wp-content/uploads/Wat-Meme-Tumblr-04.jpg" />
-                        <figcaption className="uk-overlay-panel uk-overlay-icon-feed uk-icon-usd uk-icon-spin uk-overlay-background uk-overlay-fade">
+                        <img src="http://memesvault.com/wp-content/uploads/Wat-Meme-Tumblr-04.jpg" />
+                        <figcaption className="uk-overlay-panel uk-overlay-icon-feed uk-icon-usd uk-overlay-background uk-overlay-fade">
 
                         </figcaption>
                         <a className="uk-position-cover" href="#"></a>
@@ -37,7 +37,9 @@ class ImageList extends React.Component {
 export default class Feed extends React.Component {
 	render() {
 		return (
-			<ImageList/>
+			<div id="feed">
+				<ImageList/>
+			</div>
 		);
 	}
 }
--- client/views/place_bet/PlaceBet.jsx
+++ client/views/place_bet/PlaceBet.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class PlaceBet extends React.Component {
 	render() {
 		return (
-			<div> Hi I am the place bet page </div>
+			<img className="bet-image" src="http://memesvault.com/wp-content/uploads/Wat-Meme-Tumblr-04.jpg" />
 			);
 	}
 }
