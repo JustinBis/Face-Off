8d594172e1fbd9e7e3d28e49f135027a6f6b4b80
mdimarco <mhd894@gmail.com>
2016-04-07 00:47:07 -0400

    added ridiculousness to feed

--- client/views/feed/Feed.jsx
+++ client/views/feed/Feed.jsx
@@ -8,7 +8,7 @@ class Image extends React.Component {
 				<div className="uk-thumbnail">
 					<figure className="uk-overlay">
                         <img width="600" height="400" src="http://memesvault.com/wp-content/uploads/Wat-Meme-Tumblr-04.jpg" />
-                        <figcaption className="uk-overlay-panel uk-overlay-icon-feed uk-icon-usd uk-overlay-background uk-overlay-fade">
+                        <figcaption className="uk-overlay-panel uk-overlay-icon-feed uk-icon-usd uk-icon-spin uk-overlay-background uk-overlay-fade">
 
                         </figcaption>
                         <a className="uk-position-cover" href="#"></a>
@@ -23,6 +23,9 @@ class ImageList extends React.Component {
 	render() {
 		return (
 			<div className="uk-grid">
+				<Image />
+				<Image />
+				<Image />
 				<Image />
 				<Image />
 				<Image />	
