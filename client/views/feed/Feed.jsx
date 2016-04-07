7741b1d99fdec2e7b6cbb025e0d031333fd19b94
mdimarco <mhd894@gmail.com>
2016-04-07 00:27:29 -0400

    Added uikit and updated feed page

--- client/views/feed/Feed.jsx
+++ client/views/feed/Feed.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
+
+class Image extends React.Component {
+	render() {
+		return (
+			<div className="uk-width-medium-1-3"> 
+				<div className="uk-thumbnail">
+					<figure className="uk-overlay">
+                        <img width="600" height="400" src="http://memesvault.com/wp-content/uploads/Wat-Meme-Tumblr-04.jpg" />
+                        <figcaption className="uk-overlay-panel uk-overlay-icon uk-icon-usd uk-icon-spin uk-overlay-background uk-overlay-fade">
+
+                        </figcaption>
+                        <a className="uk-position-cover" href="#"></a>
+                    </figure>
+				</div>			
+			</div>
+		);
+	}
+}
+
+class ImageList extends React.Component {
+	render() {
+		return (
+			<div className="uk-grid">
+				<Image />
+				<Image />
+				<Image />	
+			</div>
+		);
+	}
+}
+
 export default class Feed extends React.Component {
 	render() {
 		return (
-			<div> Hi I am the picture taking page </div>
-			);
+			<ImageList/>
+		);
 	}
 }
+
