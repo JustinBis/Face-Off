dc84b6873af74b30731ae448b2bd6b446dc1a0af
mdimarco <mhd894@gmail.com>
2016-04-07 20:32:08 -0400

    Betting image revamp

--- client/views/place_bet/PlaceBet.jsx
+++ client/views/place_bet/PlaceBet.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 export default class PlaceBet extends React.Component {
 	render() {
 		return (
-			<img className="bet-image" src="http://memesvault.com/wp-content/uploads/Wat-Meme-Tumblr-04.jpg" />
+			<div id="phone-body">
+				<div id="bet">
+
+				</div>
+			</div>
 			);
 	}
 }
