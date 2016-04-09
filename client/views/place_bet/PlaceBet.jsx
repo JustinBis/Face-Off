eb39233b7f2ff2d436a5236dd331377c0a6dbc15
Mason DiMarco <mhd894@gmail.com>
2016-04-08 23:55:02 -0400

    added emoji options to placing bet page

--- client/views/place_bet/PlaceBet.jsx
+++ client/views/place_bet/PlaceBet.jsx
@@ -1,6 +1,53 @@
 import React from 'react';
 
+
+
+class Choice extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="choice">
+				<img src={this.props.emojiUrl}/>
+			</div>
+		);
+	}
+}
+
+
+class ChoiceList extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+
+
+
+		return (
+			<div className="choices-container">
+				{ this.props.choices.map( (emojiUrl) => {
+					return (<Choice key={emojiUrl} emojiUrl={emojiUrl} />);
+					})
+				}
+			</div>
+		);
+	}
+}
+
+
 export default class PlaceBet extends React.Component {
+	constructor(props) {
+		super(props);
+		var choices = [ "images/emojis/toungue.png",
+						"images/emojis/kiss.png",
+						"images/emojis/open_smile.png",
+						"images/emojis/blush.png"];
+		this.state = { choices:choices };
+	}
+
 	render() {
 		return (
 			<div id="phone-body">
@@ -8,6 +55,7 @@ export default class PlaceBet extends React.Component {
 					<a className="back-btn" href="/feed">
 						<i className="uk-icon-arrow-circle-left"></i>
 					</a>
+					<ChoiceList choices={this.state.choices} />
 				</div>
 			</div>
 			);
