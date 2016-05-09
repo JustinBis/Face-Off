2df9607f114e2c0e8d7cbb7f7d2785d9894924e5
joannhuang <joannhuang@joanns-mbp.devices.brown.edu>
2016-05-08 23:48:18 -0400

    styling

--- client/views/feed/CameraTile.jsx
+++ client/views/feed/CameraTile.jsx
@@ -1,29 +1,1 @@
-import React from 'react';
-
-export default class CameraTile extends React.Component {
-
-    constructor(props) {
-        super(props);
-    }
-
-    onClick() {
-        FlowRouter.go("App.camera");
-    }
-
-    render() {
-        return (
-
-            <div id="camera-tile" className="grid-item uk-container-center"> 
-                    <div className="uk-thumbnail">
-                        <figure className="uk-overlay">
-                            <button onClick={this.onClick}><img src="images/camera.png" /></button>
-                        </figure>
-                    </div>          
-               </div>
-
-
-            );
-    }
-
-}
-
+﻿import React from 'react';export default class CameraTile extends React.Component {    constructor(props) {        super(props);    }    onClick() {        FlowRouter.go("App.camera");    }    render() {        return (            <div id="camera-tile" className="grid-item uk-container-center">                     <div className="uk-thumbnail">                        <figure className="uk-overlay">                            <button onClick={this.onClick}><img src="images/camera.png" /></button>                        </figure>                    </div>                         </div>            );    }}
